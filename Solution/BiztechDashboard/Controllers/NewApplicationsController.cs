a6be13f9ac206e8d1eadd338c3549e62209ee9f7
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-04-06 16:25:17 -0400

    up

---
+++ Solution/BiztechDashboard/Controllers/NewApplicationsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BiztechDashboard.Models;
+
+namespace BiztechDashboard.Controllers
+{
+    public class NewApplicationsController : ApiController
+    {
+        private BiztechDashboardContext db = new BiztechDashboardContext();
+
+        // GET api/NewApplications
+        public IQueryable<VW_WDSB_GetNewApp> GetVW_WDSB_GetNewApp()
+        {
+            return db.VW_WDSB_GetNewApp;
+        }
+
+    }
+}
