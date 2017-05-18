ab0a45d95407bc2f7f31ee4046b38b97a69bbfbe
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2017-05-18 11:14:55 -0400

    get auth

--- Solution/BiztechDashboard/Controllers/GetAuthController.cs
+++ Solution/BiztechDashboard/Controllers/GetAuthController.cs
@@ -6,17 +6,41 @@ using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
-
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
 namespace BiztechDashboard.Controllers
 {
     public class GetAuthController : ApiController
     {
         private BiztechDashboardContext db = new BiztechDashboardContext();
         // GET api/getauth/
-        public WDSB_GetAuth_Result Get()
+        public WDSB_GetAuth_DTO Get()
         {
-            return (from i in db.WDSB_GetAuth(getMyuserName(),"Maintenance")
-                   select i).First();
+            string givenName = System.DirectoryServices.AccountManagement.UserPrincipal.Current.GivenName;
+            string surname = System.DirectoryServices.AccountManagement.UserPrincipal.Current.Surname;
+            string fullname = givenName.Trim() + " " + surname.Trim();
+            var tempList = (from i in db.WDSB_GetAuth(getMyuserName(),"Maintenance")
+                   select i).ToList();
+            if (tempList.Count() == 0)
+                return new WDSB_GetAuth_DTO
+                {
+                    CanAdd = false,
+                    CanDelete = false,
+                    CanEdit = false,
+                    CanView = false,
+                    FullName = fullname,
+                    UserName=getMyuserName()
+                };
+            var temp = tempList[0];
+            return new WDSB_GetAuth_DTO
+            {
+                CanAdd = temp.CanAdd,
+                CanDelete = temp.CanDelete,
+                CanEdit = temp.CanEdit,
+                CanView = temp.CanView,
+                FullName = fullname,
+                UserName = temp.UserName
+            };
         }
 
         private string getMyuserName()
@@ -24,6 +48,8 @@ namespace BiztechDashboard.Controllers
             string currentDomainUser = HttpContext.Current.User.Identity.Name.ToString();
             //username only
             string currentUsername = currentDomainUser.Remove(0, currentDomainUser.IndexOf('\\') + 1);
+                //WindowsIdentity.GetCurrent().Name;
+            //Console.WriteLine(WindowsIdentity.GetCurrent().Name);
             //int index = currentDomainUser.IndexOf("\\" + currentUsername);
             //Domain Name only
             //string currentDomainname = (index < 0) ? currentDomainUser : currentDomainUser.Remove(index, currentUsername.Length + 1);
---
+++ Solution/BiztechDashboard/Models/WDSB_GetAuth_DTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BiztechDashboard.Models
+{
+    public class WDSB_GetAuth_DTO
+    {
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public Nullable<bool> CanAdd { get; set; }
+        public Nullable<bool> CanDelete { get; set; }
+        public Nullable<bool> CanEdit { get; set; }
+        public Nullable<bool> CanView { get; set; }
+    }
+}
